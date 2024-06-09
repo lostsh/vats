da064ad183300875dbb981aa5a4c2133fddb5779
lostsh <yohann.vernhes@proton.me>
2024-06-09 10:46:05 +0200

    json module playground

--- src/main.go
+++ src/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    //"os" //to check input args
+    "slices"
+    "encoding/json"
+)
 
 func main(){
-    fmt.Println("Hi this is Vulnerability Assets Tracking System")
+    fmt.Println("Vulnerability Assets Tracking System")
+
+    vulns := []Vuln{
+        Vuln{Component: "JQuery", Version: "0.23", Criticity: 8, Comments: "c"},
+        Vuln{Component: "React", Version: "0.2", Criticity: 4.3, Comments: "c"},
+        Vuln{Component: "Apache", Version: "0.2", Criticity: 9, Comments: "c"},
+        Vuln{Component: "Fraise", Version: "0.2", Criticity: 1.1, Comments: "c"},
+    }
+
+    fmt.Println(vulns)
+
+    slices.SortFunc(vulns, CompareVuln)
+    fmt.Println(vulns)
+
+    byteJson, _ := json.Marshal(vulns)
+    fmt.Println(string(byteJson))
+    arryunsze := []Vuln{}
+    json.Unmarshal(byteJson, &arryunsze)
+    fmt.Println(arryunsze)
+
+    serialize_test()
+
+    fmt.Println("New Tests :")
+
+    //m := make(map[int]Vuln)
+    m := map[int]Vuln{
+        1:Vuln{Component: "JQuery", Version: "0.23", Criticity: 8, Comments: "c"},
+        2:Vuln{Component: "React", Version: "0.2", Criticity: 4.3, Comments: "c"},
+        3:Vuln{Component: "Apache", Version: "0.2", Criticity: 9, Comments: "c"},
+        4:Vuln{Component: "Fraise", Version: "0.2", Criticity: 1.1, Comments: "c"},
+    }
+
+    fmt.Println(m)
+
+    jsonmap, _ := json.Marshal(m)
+    fmt.Println(string(jsonmap))
+    unjsonmap := make(map[int]Vuln)
+    json.Unmarshal(jsonmap, &unjsonmap)
+    fmt.Println(unjsonmap)
 }
---
+++ src/serializer.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+)
+
+
+func serialize_test(){
+
+	readFile("/tmp/fraise.txt")
+
+}
